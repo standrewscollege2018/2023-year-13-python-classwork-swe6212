5dad84c4ae0a765f2455f4619210f914fe6c05b3
swe6212 <78330796+swe6212@users.noreply.github.com>
2023-08-09 09:50:46 +1200

    getting teachers code because mine did not work

--- introdution.py
+++ introdution.py
@@ -1,74 +1,85 @@
-'''Student mangement system ''' 
+''' Student management system '''
 
 class Student:
+    ''' Student objects have attributes like name, phone, address, and a list of
+    the classes they are enrolled in. There are also getter functions for each,
+    as well as functions to display all of their information. '''
 
     def __init__(self, name, age, phone, classes):
+        ''' The init function is called automatically when the object is created'''
 
-            # Print Hello
-            print("Hello")
+        print("Hello")
 
-            # Assign name to new student
-            # All properties have an underscore in front 
-            # This means the property is prive to the object
-            # And they can only be referenced by calling a getter 
-            # Function from the object (encapsulation)
-            self._name = name
-            self._age = age
-            self._phone = phone
-            self._classes = classes 
+        # Assign properties to new student
+        # All properties have an underscore in front
+        # This means the property is private to the object
+        # and they can only be referenced by calling a getter
+        # function from the object (encapsulation)
+        self._name = name
+        self._phone = phone
+        self._age = age
+        self._classes = classes
 
-            # Student is automatically enrolled 
-            self._enrol = True 
+        # Student is automatically enrolled
+        self._enrolled = True
 
-            # Add the new student to the students list
-            students.append(self)
-
-
+        # Add the new student to the students list
+        students.append(self)
+    
+    
     def get_name(self):
-        ''' return the name of the student '''
+        ''' Return the name of the student '''
+
         return self._name
     
-    def get_age(self):
-          ''' return the age of the student'''
-          return self._age
-    
     def get_phone(self):
-          ''' return the phone number of the student '''
-          return self._phone
+        ''' Return the phone number '''
+
+        return self._phone
+
+    def get_age(self):
+        ''' Return their age '''
+
+        return self._age
     
     def get_classes(self):
-          ''' return the classes of the student'''
-          return self._classes
+        ''' Return list of classes they are enrolled in '''
+
+        return self._classes
     
-    def get_enrol(self):
-          ''' return the enrol of the students'''
-          return self._enrol
-    
+    def get_enrolled(self):
+        ''' Return enrolment status '''
+
+        return self._enrolled
+
     def unenrol(self):
-        ''' this is a setter funtion that changes the value of a '''
+        ''' This is a setter function that changes the value of a property '''
 
-        self._enrol = False
+        self._enrolled = False
 
-        
-        def display_my_info(self):
-              ''' this displays all infoamtion about the student '''
-              print("=" * 30)
-              print(f"Name: {self._name}")
-              print(f"Age: {self._age}")
-              print(f"phone: {self._phone}")
-              print(f"classes:", end=" ")
-              for c in self._classes:
-                        print(c, end = " ")
-                        print()
-              print("=" * 30)
+    def display_my_info(self):
+        ''' This displays all information about the student '''
 
-# List to store all students 
+        print("=" * 30)
+        print(f"Name: {self._name}")
+        print(f"Age: {self._age}")
+        print(f"Phone: {self._phone}")
+        # Display all classes on one line
+        print(f"Classes: ", end=" ")
+        for c in self._classes:
+            print(c, end=" ")
+        print()
+        print("=" * 30)
+
+# list to store all students
 students = []
 
-# Create student
-Student("John Smith", 18, "021 123 987", ["DIGI", "MATH"])
-Student("Joanna Smith", 17, "027 987 123", ["SIENC", "ECO"])
+# Create students
+Student("John Smith", 18, "027123123", ["DIGI", "MATH"])
+Student("Joanna Smith", 17, "021321321", ["DIGI", "BIOL"])
 
 def search():
-      ''' user searches for student'''
-      name_search = input("Enter name to search for: ")
+    ''' User searches for student '''
+
+    name_search = input("Enter name to search for: ")
+    

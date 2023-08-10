3a637400eec2c7c78791f67f6f21c275f2558fc8
swe6212 <78330796+swe6212@users.noreply.github.com>
2023-08-10 12:23:58 +1200

    Mr adims code from his github to get the students from the class code

--- introdution.py
+++ introdution.py
@@ -8,7 +8,7 @@ class Student:
     def __init__(self, name, age, phone, classes):
         ''' The init function is called automatically when the object is created'''
 
-        # Assign properties to new student
+         # Assign properties to new student
         # All properties have an underscore in front
         # This means the property is private to the object
         # and they can only be referenced by calling a getter
@@ -79,23 +79,25 @@ Student("Joanna Smith", 17, "021321321", ["DIGI", "BIOL"])
 def search():
     ''' User searches for student '''
 
-      #user enters name to search for
+    # User enters name to search for
     name_search = input("Enter name to search for: ")
-    #loop through lists of studentes
-    #cheack if the name of a student matches name_search
-    #if so, display their details
-
+    # Loop through list of students
+    # Check if the name of a student matches name_search
+    # If so, display their details
     for s in students:
-      #for an exact match use: if name_search == s.get_name():
-      #for a search matching any characters use: if name_search in s.get_name():
-        if name_search.lower()+" " in s.get_name().lower() or " " +name_search.lower() in s.get_mame().lower():
+        # for an exact match use: if name_search == s.get_name():
+        # for a search matching any characters use: if name_search in s.get_name():
+
+        # This line returns results if either first or last name matches
+        if name_search.lower()+" " in s.get_name().lower() or " "+name_search.lower() in s.get_name().lower():
             s.display_my_info()
 
-def display_all():
-      ''' Display names of all studentds'''
 
-      for s in students:
-          s.display_my_info()
+def display_all():
+    ''' Display names of all students '''
+
+    for s in students:
+        s.display_my_info()
 
 def add_student():
     ''' User can add a new student '''
@@ -103,27 +105,48 @@ def add_student():
     print("Enter new student details")
     print("-"*20)
     name = input("Name: ")
-    #put error catching  around age (use try and exept)
+    # Put error catching around age (use try and except)
     enter_age = True
     while enter_age:
         try:
-            age= int(input("Age: "))
+            age = int(input("Age: "))
             enter_age = False
         except ValueError:
             print("Enter an integer")
-            phone = input("PHone: ")
-            student_classes = []
-            enter_classes = True
-            while enter_class:
-                  new_class = input("Class code (end to stop): ")
-                  if new_class == "end":
-                        #user has finished entering classes
-                        enter_class = False
-            else: 
-                  #add class code to list of classes
-                  student_classes.append(new_class)
-            #create new student
-            Student(name, age, phone, student_classes)
-                
-add_student()
+    phone = input("Phone: ")
+    student_classes = []
+    enter_class = True
+    while enter_class:
+        new_class = input("Class code(end to stop): ")
+        if new_class == "end":
+            # user has finished entering classes
+            enter_class = False
+        else:
+            # add class code to list of classes
+            student_classes.append(new_class)
+    # Create new student
+    Student(name, age, phone, student_classes)
+
+def generate_students():
+    ''' Function imports students from csv file '''
+
+    import csv
+    with open('myRandomStudents.csv', newline='') as csvfile:
+        filereader = csv.reader(csvfile)
+        for line in filereader:
+            classes = []
+            i = 3
+            while i in range(3,8):
+                classes.append(line[i])
+                i += 1
+            Student(line[0], int(line[1]), line[2], classes)
+
+
+# Create a function that gets the user to enter a class code, then
+# returns a list of all students who are in that class, along with a 
+# count of how many there are
+
+
+
+generate_students()
 search()
